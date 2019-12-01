dc6a2f5c818ef3c3ba852065907c7d0915283d59
emregurler <emregurler93@gmail.com>
2019-12-01 18:26:22 +0300

    Protected route example add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {
+  Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import './App.css'
 
-function App() {
+const ProtectedRoute = ({
+  component: Component,
+  componentProps,
+  isAuthenticated,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated ? (
+        <Component {...props} {...componentProps} />
+      ) : (
+        <Redirect to='/login' />
+      )
+    }
+  />
+)
+
+const ProtectedPage = ({ onClickLogout }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div>PROTECTED PAGE</div>
+      <button type='button' onClick={onClickLogout}>
+        LOGOUT
+      </button>
+    </React.Fragment>
+  )
 }
 
-export default App;
+const LoginPage = ({ onClickAuthenticateButton }) => {
+  return (
+    <React.Fragment>
+      <span>Welcome my dummy LOGIN PAGE Click button for authenticate</span>
+      <div>
+        <button type='button' onClick={onClickAuthenticateButton}>
+          Authenticate
+        </button>
+      </div>
+    </React.Fragment>
+  )
+}
+
+const NormalPage = (props) => <div>NORMAL PAGE</div>
+
+const App = () => {
+  const token = localStorage.getItem('token')
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    token && token.length > 0
+  )
+
+  const onClickLogout = (e) => {
+    setIsAuthenticated(false)
+    localStorage.clear()
+  }
+  const onClickAuthenticateButton = (e) => {
+    localStorage.setItem('token', 'Bearer Valid_TOKEN')
+    setIsAuthenticated(true)
+  }
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path='/login'
+            render={(props) => (
+              <React.Fragment>
+                {isAuthenticated ? (
+                  <Redirect to='/' />
+                ) : (
+                  <LoginPage
+                    {...props}
+                    onClickAuthenticateButton={onClickAuthenticateButton}
+                  />
+                )}
+              </React.Fragment>
+            )}
+          />
+          <Route exact path='/public' component={NormalPage} />
+          <ProtectedRoute
+            componentProps={{ onClickLogout }}
+            isAuthenticated={isAuthenticated}
+            component={ProtectedPage}
+          />
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
